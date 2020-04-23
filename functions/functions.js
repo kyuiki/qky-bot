e24a3b07721fb3a8a689df76786be346c11a1429
qky <qkie0915@gmail.com>
2020-04-23 21:17:45 +0700

    disabled nsfw

--- functions/functions.js
+++ functions/functions.js
@@ -1,6 +1,13 @@
-exports.run = async (client, channelID, fnName, data1, data2, data3) => {
+/*exports.run = async (client, channelID, fnName, data1, data2, data3) => {
      const channel = client.channels.cache.get(channelID);
      if (!channel) return console.log('err not found channel named bot-channel');
+     switch(fnName){
+     	case 'detectNewUser':
+     	detectNewUser(data1);
+     	break;
+     	case '';
+     	break;
+     }
 
 
 	function detectNewUser(a, b, c){
@@ -14,4 +21,4 @@ exports.run = async (client, channelID, fnName, data1, data2, data3) => {
   		timeDif = `Akun dibuat : ${createAt.toUTCString()}\nBergabung : ${joinedAt.toUTCString()}\n ${days} hari, ${hours} jam, ${minutes} menit, ${seconds} detik`;
   		console.log(timeDif)
 	}
-}
+}*/
